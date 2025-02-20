35a0d3fe08030a5d2288730122fe9d215b658b0a
Shashank <shashankshashidhar73@gmail.com>
2025-02-20 16:22:30 +0530

    added dashboardRoutes middleware

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const path = require('path');
+const dashboardRoutes = require('./routes/dashboardsRoutes');
 
 dotenv.config();
 connectDB();
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/auth', authRoutes);
+app.use('/dashboard',dashboardRoutes);
 app.get('/', (req, res) => res.redirect('/api/auth/login'));
 
 const PORT = process.env.PORT || 5000;
