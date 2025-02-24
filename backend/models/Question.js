d9f32a90f35c86c791e717485b6ae57ae6d977cb
Shashank <shashankshashidhar73@gmail.com>
2025-02-24 19:52:32 +0530

    add answers for questionSchema

--- backend/models/Question.js
+++ backend/models/Question.js
@@ -4,7 +4,8 @@ const questionSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     title: { type: String, required: true },
     description: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
+    createdAt: { type: Date, default: Date.now },
+    answers: [String]
 });
 
 module.exports = mongoose.model('Question', questionSchema);
