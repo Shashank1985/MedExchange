ed410f1d5cf1fa771adc031c39747515c744580b
Shashank <shashankshashidhar73@gmail.com>
2025-03-01 23:20:28 +0530

    added new answer schema

--- backend/models/Answer.js
+++ backend/models/Answer.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const answerSchema = new mongoose.Schema({
     questionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Question', required: true },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    text: { type: String, required: true },
+    answer: { type: String, required: true },
     createdAt: { type: Date, default: Date.now }
 });
 
