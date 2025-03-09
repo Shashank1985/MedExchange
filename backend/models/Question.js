62a33175c6b4271725a6badd77984ee3c8be0152
Shashank <shashankshashidhar73@gmail.com>
2025-03-09 16:47:09 +0530

    modify question model to stores3 url for image uploads

--- backend/models/Question.js
+++ backend/models/Question.js
@@ -4,8 +4,8 @@ const questionSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     title: { type: String, required: true },
     description: { type: String, required: true },
+    imageUrl: {type: String},
     createdAt: { type: Date, default: Date.now },
-    answers: [String]
 });
 
 module.exports = mongoose.model('Question', questionSchema);
