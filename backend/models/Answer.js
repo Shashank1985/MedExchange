fe5cf52a26ec3f0efbf0e7319c5755b46b585417
Shashank <shashankshashidhar73@gmail.com>
2025-05-08 21:18:48 +0530

    change Answer user to store answer ids

--- backend/models/Answer.js
+++ backend/models/Answer.js
@@ -4,7 +4,8 @@ const answerSchema = new mongoose.Schema({
     questionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Question', required: true },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     answer: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
+    createdAt: { type: Date, default: Date.now },
+    parentAnswerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Answer', default: null, index: true}
 });
 
 module.exports = mongoose.model('Answer', answerSchema);
